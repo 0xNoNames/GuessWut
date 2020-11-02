46d0404cd7090e48e29d76963554a7a6eef39204
Arthur Pêtre <arthur30700@gmail.com>
2020-11-02 08:44:10 +0100

    Renvoie des images différentes à chaques fois

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ const io = require('socket.io')(server);
 const userPointsMap = new Map();
 var pixel_state = 0;
 var game_state = 1;
-var imgs_array = ['siphano', 'chien', 'chien1', 'cheval', 'val', 'arthur', 'merenathan.jpg', 'homer', 'nunu', 'porot', 'bowser', 'laink', 'boa', 'chat', 'chat1', 'caisse café', 'svastika', 'porot1', 'jerry','sylvain durif', 'skyyart', 'alain soral', 'chien2','poule','chien3','hitler','marine le pen', 'chien4','chien5','laink1','rammus','fien','braum','chat2','porot2','porot3','porot4','porot5','nathan','chauve-souris','arthur1','^^','chat3','sardoche','wartek','shrek','porot6','porot7','gateau','chat4','nathan1','t-rex','sardoche1']
-var img_name = imgs_array[getRandomInt(imgs_array.length - 1)];
+var imgs_array = ['siphano', 'chien', 'chien1', 'cheval', 'val', 'arthur', 'merenathan.jpg', 'homer', 'nunu', 'porot', 'bowser', 'laink', 'boa', 'chat', 'chat1', 'caisse café', 'svastika', 'porot1', 'jerry', 'sylvain durif', 'skyyart', 'alain soral', 'chien2', 'poule', 'chien3', 'hitler', 'marine le pen', 'chien4', 'chien5', 'laink1', 'rammus', 'fien', 'braum', 'chat2', 'porot2', 'porot3', 'porot4', 'porot5', 'nathan', 'chauve-souris', 'arthur1', '^^', 'chat3', 'sardoche', 'wartek', 'shrek', 'porot6', 'porot7', 'gateau', 'chat4', 'nathan1', 't-rex', 'sardoche1']
+var copy_array = imgs_array.slice();
+
+var random_nb = getRandomInt(imgs_array.length - 1);
+var img_name = copy_array[random_nb];
+copy_array.splice(copy_array.indexOf(random_nb));
 
 
 ////////////////////////////////
@@ -21,7 +25,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-server.listen(process.env.PORT)
+server.listen(process.env.PORT);
 
 
 //////////////////////////////////
@@ -37,15 +41,26 @@ function restartGame(ws, trouved = 1) {
   pixel_state = 0;
   pixel(0);
   if (trouved) {
-    io.emit('message', ws.username + " à TROUVED !!!!" + " ct " + img_name.replace(/\d+$/, ""))
+    io.emit('message', ws.username + " à TROUVED !!!!" + " ct " + img_name.replace(/\d+$/, ""));
     userPointsMap.set(ws.username, ++ws.point);
     io.emit('update', JSON.stringify(Array.from(userPointsMap)));
-  } else io.emit('message', "vous êtes naze, c'était " + img_name.replace(/\d+$/, "") + " :/")
-  img_name = imgs_array[getRandomInt(imgs_array.length - 1)];
+    random_nb = getRandomInt(copy_array.length - 1);
+    img_name = copy_array[random_nb];
+    copy_array.splice(random_nb);
+  } else {
+    io.emit('message', "vous êtes naze, c'était " + img_name.replace(/\d+$/, "") + " :/");
+    random_nb = getRandomInt(copy_array.length - 1);
+    img_name = copy_array[random_nb];
+  }
+
+  if (copy_array.length == 0) {
+    copy_array = imgs_array.slice();
+  }
+
   setTimeout(() => {
     interval_img_guess = setInterval(() => pixel(), 100)
-    io.emit('message', "");
-    game_state = 1
+    io.emit('message', '');
+    game_state = 1;
   }, 3000);
 }
 
@@ -68,7 +83,7 @@ function pixel(bool = 1) {
         });
       });
     } else if (pixel_state > 100) {
-      restartGame("", false)
+      restartGame("", false);
     } else {
       Jimp.read(__dirname + '/public/images/' + img_name + '.jpg').then(image => {
         pixel_state += 0.25;
@@ -95,7 +110,7 @@ function pixel(bool = 1) {
 
 var interval_img_guess = setInterval(() => {
   pixel();
-}, 100)
+}, 100);
 
 io.on('connection', (ws) => {
 
@@ -116,8 +131,8 @@ io.on('connection', (ws) => {
 
   ws.on('guess', (msg) => {
     if (game_state) {
-      if (msg == img_name.replace(/\d+$/, "")) restartGame(ws)
-      else ws.emit('message', "Pas trouved")
+      if (msg == img_name.replace(/\d+$/, "")) restartGame(ws);
+      else ws.emit('message', "Pas trouved");
     }
-  })
+  });
 });
